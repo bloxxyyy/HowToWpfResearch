df9852cb8bb050c8f6bbb0d229ad1a661d42625d
Marco <mn.noppen@hotmail.com>
2025-07-15 21:06:50 +0200

    Example with DI and MvvmToolkit

---
+++ HowToWpfResearch/Src/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace HowToWpfResearch.Src.ViewModels;
+
+public partial class MainViewModel : ObservableObject {
+
+    [ObservableProperty]
+    private string greeting = "Hello, WPF + CommunityToolkit!";
+
+    [RelayCommand]
+    private void UpdateGreeting() {
+        Greeting = "You clicked the button!";
+    }
+
+}
