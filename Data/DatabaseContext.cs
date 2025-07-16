4ef487aee5a812d40c83d222eaccc44ac31c1f08
Marco <mn.noppen@hotmail.com>
2025-07-17 00:16:08 +0200

    Added; ci-tools which contains a docker-compose and helperscripts to run a MSSql database; added enviroment variables and setup a Data project to contain Entities and Repositories; finally added a migration project to run DB migrations on running the database in docker;

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options) {
+    public DbSet<PersonEntity> PersonEntity => Set<PersonEntity>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        modelBuilder.Entity<PersonEntity>().ToTable("People");
+    }
+}
---
+++ DbMigrator/Program.cs
@@ -0,0 +1,27 @@
+﻿using Data;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+IHost host = Host.CreateDefaultBuilder()
+    .ConfigureServices((context, services) => {
+
+        string password = Environment.GetEnvironmentVariable("SA_PASSWORD") ?? throw new Exception("Enviroment not set for database!");
+        string database = Environment.GetEnvironmentVariable("DB_NAME")     ?? throw new Exception("Enviroment not set for database!");
+        string user     = Environment.GetEnvironmentVariable("DB_USER")     ?? throw new Exception("Enviroment not set for database!");
+
+        string connectionString = $"Server=localhost,1433;Database={database};User Id={user};Password={password};TrustServerCertificate=True";
+
+        services.AddDbContext<DatabaseContext>(options =>
+            options.UseSqlServer(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure()));
+        })
+
+    .Build();
+
+using IServiceScope scope = host.Services.CreateScope();
+DatabaseContext db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+
+Console.WriteLine("Applying migrations...");
+await db.Database.MigrateAsync();
+Console.WriteLine("Done.");
